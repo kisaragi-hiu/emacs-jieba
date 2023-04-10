438e32aaed9511ad07fab85e6dc7e42ed52f626f
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-04-10 10:51:03 +0900

    It actually compiles and works now

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use emacs::{defun, Env, Result, Value, Vector};
+use emacs::{defun, Env, IntoLisp, Result, Value, Vector};
 use jieba_rs::{Jieba, KeywordExtract, TFIDF};
 use once_cell::sync::OnceCell;
 use std::str;
@@ -6,103 +6,101 @@ use std::str;
 emacs::plugin_is_GPL_compatible!();
 
 static JIEBA: OnceCell<Jieba> = OnceCell::new();
-static TFIDF_INSTANCE: OnceCell<Jieba> = OnceCell::new();
+static TFIDF_INSTANCE: OnceCell<TFIDF> = OnceCell::new();
 
-#[emacs::module(name = "jieba")]
-fn init(env: &Env) -> Result<()> {
-    Ok(())
+// From emacs-tree-sitter
+// https://github.com/emacs-tree-sitter/elisp-tree-sitter/blob/master/core/src/query.rs#L13
+fn vec_to_vector<'e, T: IntoLisp<'e>>(env: &'e Env, vec: Vec<T>) -> Result<Vector<'e>> {
+    let vector = env.make_vector(vec.len(), ())?;
+    for (i, v) in vec.into_iter().enumerate() {
+        vector.set(i, v)?;
+    }
+    Ok(vector)
 }
 
-#[inline]
-fn assert_not_init() -> Result<()> {
-    if JIEBA.get().is_some() {
-        Err(Error::from_reason(
-            "Jieba has already been loaded, cannot load again".to_owned(),
-        ))
-    } else {
-        Ok(())
-    }
+#[emacs::module(name = "jieba")]
+fn init(_env: &Env) -> Result<()> {
+    Ok(())
 }
 
 /// Initialize Jieba, or do nothing if it is already initialized.
 #[defun]
 fn load() -> Result<()> {
-    assert_not_init()?;
     let _ = JIEBA.get_or_init(Jieba::new);
     Ok(())
 }
 
 /// Cut SENTENCE into an array of tokens.
 #[defun]
-fn cut(env: &Env, sentence: String, hmm: Option<Value>) -> Result<Vector<String>> {
+fn cut<'e>(env: &'e Env, sentence: String, hmm: Option<Value>) -> Result<Vector<'e>> {
     let jieba = JIEBA.get_or_init(Jieba::new);
     let cutted = jieba.cut(sentence.as_str(), hmm.is_some());
-    Ok(env.vector(cutted))
+    vec_to_vector(env, cutted)
 }
 
 #[defun]
-fn cut_all(env: &Env, sentence: String) -> Result<Vector<String>> {
+fn cut_all(env: &Env, sentence: String) -> Result<Vector> {
     let jieba = JIEBA.get_or_init(Jieba::new);
     let cutted = jieba.cut_all(sentence.as_str());
-    Ok(env.vector(cutted))
+    vec_to_vector(env, cutted)
 }
 
 #[defun]
-fn cut_for_search(env: &Env, sentence: String, hmm: Option<Value>) -> Result<Vector<String>> {
+fn cut_for_search<'e>(env: &'e Env, sentence: String, hmm: Option<Value>) -> Result<Vector<'e>> {
     let jieba = JIEBA.get_or_init(Jieba::new);
     let cutted = jieba.cut_for_search(sentence.as_str(), hmm.is_some());
-    Ok(env.vector(cutted))
+    vec_to_vector(env, cutted)
 }
 
-struct TaggedWord {
-    pub tag: String,
-    pub word: String,
-}
+// struct TaggedWord {
+//     pub tag: String,
+//     pub word: String,
+// }
 
-#[defun]
-fn tag(env: &Env, sentence: String, hmm: Option<Value>) -> Result<Vector<TaggedWord>> {
-    let jieba = JIEBA.get_or_init(Jieba::new);
-    let tagged = jieba.tag(sentence.as_str(), hmm.is_some());
+// #[defun]
+// fn tag(env: &Env, sentence: String, hmm: Option<Value>) -> Result<Vector> {
+//     let jieba = JIEBA.get_or_init(Jieba::new);
+//     let tagged = jieba.tag(sentence.as_str(), hmm.is_some());
 
-    Ok(tagged
-        .iter()
-        .map(|t| TaggedWord {
-            tag: t.tag.to_owned(),
-            word: t.word.to_owned(),
-        })
-        .collect())
-}
+//     Ok(tagged
+//         .iter()
+//         .map(|t| TaggedWord {
+//             tag: t.tag.to_owned(),
+//             word: t.word.to_owned(),
+//         })
+//         .collect())
+// }
 
-struct Keyword {
-    pub keyword: String,
-    pub weight: f64,
-}
+// struct Keyword {
+//     pub keyword: String,
+//     pub weight: f64,
+// }
 
-#[defun]
-fn extract(sentence: String, topn: u32, allowed_pos: Option<String>) -> Result<Vector<Keyword>> {
-    let allowed_pos_string = allowed_pos.unwrap_or_else(|| "".to_owned());
+// #[defun]
+// fn extract(sentence: String, topn: u32, allowed_pos: Option<String>) -> Result<Vector> {
+//     let allowed_pos_string = allowed_pos.unwrap_or_else(|| "".to_owned());
 
-    let allowed_pos: Vec<String> = if allowed_pos_string.is_empty() {
-        vec![]
-    } else {
-        allowed_pos_string
-            .split(',')
-            .map(|s| s.to_owned())
-            .collect()
-    };
+//     let allowed_pos: Vec<String> = if allowed_pos_string.is_empty() {
+//         vec![]
+//     } else {
+//         allowed_pos_string
+//             .split(',')
+//             .map(|s| s.to_owned())
+//             .collect()
+//     };
 
-    let keyword_extractor = TFIDF_INSTANCE.get_or_init(|| {
-        let jieba = JIEBA.get_or_init(Jieba::new);
-        TFIDF::new_with_jieba(jieba)
-    });
+//     let keyword_extractor = TFIDF_INSTANCE.get_or_init(|| {
+//         let jieba = JIEBA.get_or_init(Jieba::new);
+//         TFIDF::new_with_jieba(jieba)
+//     });
 
-    let tags = keyword_extractor.extract_tags(sentence.as_str(), topn as usize, allowed_pos);
+//     let tags = keyword_extractor.extract_tags(sentence.as_str(), topn as usize, allowed_pos);
 
-    Ok(tags
-        .into_iter()
-        .map(|tag| Keyword {
-            keyword: tag.keyword,
-            weight: tag.weight,
-        })
-        .collect::<Vector<Keyword>>())
-}
+//     Ok(tags
+//         .into_iter()
+//         .map(|tag| Keyword {
+//             keyword: tag.keyword,
+//             weight: tag.weight,
+//         })
+//         .collect::<Vector>())
+// }
