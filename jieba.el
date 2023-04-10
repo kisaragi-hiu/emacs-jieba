05e116eedecb8edbd576fb53be137cd072c45a9b
Kisaragi Hiu <mail@kisaragi-hiu.com>
2023-04-10 22:27:07 +0900

    Build on load

--- jieba.el
+++ jieba.el
@@ -4,8 +4,8 @@
 ;; Maintainer: Kisaragi Hiu
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "25.1"))
-;; Homepage: homepage
-;; Keywords: keywords
+;; Homepage: https://github.com/kisaragi-hiu/emacs-jieba
+;; Keywords: extensions
 
 
 ;; This file is not part of GNU Emacs
@@ -29,6 +29,130 @@
 ;; commentary
 
 ;;; Code:
+
+(require 'dash)
+(require 'ansi-color)
+(require 'json)
+
+(defgroup jieba nil
+  "Emacs Lisp bindings for jieba-rs."
+  :group 'extensions)
+
+(defcustom jieba-dyn-dir (file-name-directory
+                          (or (locate-library "jieba-dyn")
+                              (locate-library "jieba")
+                              load-file-name
+                              buffer-file-name))
+  "The directory where the `jieba-dyn' module is, or should be.
+
+This needs to be set before `jieba' is loaded."
+  :group 'jieba
+  :type 'directory)
+
+(defcustom jieba-dyn-get-method (list "todo" 'compile)
+  "How to get the dynamic module for jieba, if necessary.
+
+This needs to be set before `jieba' is loaded.
+
+A string means to download it from this URL.
+
+The symbol `compile' means to automatically compile it with a
+local Rust toolchain."
+  :group 'jieba
+  :type '(set (choice
+               (const :tag "Binary from GitHub" "todo")
+               (string :tag "Another URL"))
+              (const :tag "Local compilation" 'compile)))
+
+(defmacro jieba--with-temp-dir (&rest body)
+  "BODY with `tmp-dir' bound to a new temporary directory.
+BODY needs to take care of deleting `tmp-dir' itself."
+  (declare (indent 0))
+  `(let ((tmp-dir (make-temp-file "emacs-jieba-build" t)))
+     ,@body))
+
+(defun jieba--dyn-get-ensure ()
+  "Ensure `jieba-dyn' is available by downloading it or building it."
+  (unless (executable-find "cargo")
+    (error "Rust toolchain is not available. Make sure `cargo' is in your path"))
+  (let ((load-path (cons jieba-dyn-dir load-path)))
+    (unless (locate-library "jieba-dyn")
+      (or (and (-some #'stringp jieba-dyn-get-method)
+               (jieba--dyn-download
+                (-first #'stringp jieba-dyn-get-method)))
+          (and (memq 'compile jieba-dyn-get-method)
+               (jieba--dyn-build)))
+      ;; The library should be available now. If not, signal.
+      (unless (locate-library "jieba-dyn")
+        (error "Could not ensure `jieba-dyn' is available")))
+    t))
+
+(defun jieba--dyn-download (_url)
+  "Download the built dynamic module from URL."
+  nil)
+
+(defun jieba--dyn-build ()
+  "Build the dynamic library."
+  (jieba--with-temp-dir
+    (copy-directory "." tmp-dir nil t)
+    (let ((default-directory tmp-dir)
+          (buf (pop-to-buffer (get-buffer-create "*cargo build*")))
+          pipe)
+      (with-current-buffer buf
+        (compilation-mode))
+      (setq
+       pipe
+       (make-pipe-process
+        :name "cargo output"
+        :buffer nil
+        :filter (lambda (_process output)
+                  (with-current-buffer buf
+                    (let ((inhibit-read-only t))
+                      (goto-char (point-max))
+                      (insert (ansi-color-apply (format "%s" output))))))
+        :sentinel (lambda (process _change)
+                    (when (eq 'closed (process-status process))
+                      (kill-buffer buf)))))
+      (let ((json-buf (generate-new-buffer " *json*"))
+            (output-file (expand-file-name (concat "jieba-dyn" module-file-suffix)
+                                           jieba-dyn-dir))
+            process
+            cargo-output-file)
+        (setq process
+              (make-process
+               :name "cargo"
+               :stderr pipe
+               :command `("cargo" "build" "-r"
+                          "--color" "always"
+                          "--message-format" "json")
+               :sentinel (lambda (process _change)
+                           (when (eq 'exit (process-status process))
+                             (message "Compiled %s" output-file)
+                             (kill-buffer json-buf)
+                             (delete-directory tmp-dir t)))
+               :filter
+               (lambda (_process output)
+                 (with-current-buffer json-buf
+                   (insert (format "%s" output))
+                   (goto-char (point-min))
+                   (while (search-forward "\n" nil t)
+                     (goto-char (point-min))
+                     (let ((json (ignore-errors (json-read))))
+                       (when json
+                         (let-alist json
+                           (when (and (equal .reason "compiler-artifact")
+                                      (equal .target.name "emacs-jieba"))
+                             (setq cargo-output-file (elt .filenames 0))))))
+                     (delete-region (point-min) (point))))
+                 (when cargo-output-file
+                   (copy-file cargo-output-file
+                              output-file
+                              :ok)))))
+        ;; Block until it's done.
+        (while (accept-process-output process)
+          (redisplay))))))
+
+(jieba--dyn-get-ensure)
 
 (require 'jieba-dyn)
 
